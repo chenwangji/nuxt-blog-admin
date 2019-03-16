106cf57d5c3907e7d7c903a92a9fe000bd25a4f5
chenwangji <694093637@qq.com>
2019-03-17 00:56:51 +0800

    feat: 更新正式环境地址

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@
 
 // cors - test
 const IS_DEV: boolean = process.env.NODE_ENV !== 'production'
-export const API_ROOT: string = IS_DEV ? 'http://localhost:8000/api/' : window.location.origin + '/api/'
+export const API_ROOT: string = IS_DEV ? 'http://localhost:8000/api/' : 'http://admin.todyto.cn/api/'
 
 /**
  * 七牛对象存储空间 测试域名
